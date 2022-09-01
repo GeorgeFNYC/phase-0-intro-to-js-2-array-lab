221f29a97aa1aef3b26f36d1ff6a9b0283c2010f
George Fernandez <fernandezg704@gmail.com>
2022-09-01 18:53:08 -0400

    commit tree 9d20fdca47a6af484c986cdb5b15734721f585aa

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat () {
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat () {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    return cats.shift();
+}
+
+function appendCat () {
+    let newLastCats = [...cats, 'Broom'];
+    return newLastCats;
+}
+
+function prependCat () {
+    let newStartCats = ['Arnold', ...cats];
+    return newStartCats;
+}
+
+function removeLastCat () {
+    let removedLastCat = cats.slice(0,2);
+    return removedLastCat;
+}
+
+function removeFirstCat () {
+    let removedFirstCat = cats.slice(1,3);
+    return removedFirstCat
+}
